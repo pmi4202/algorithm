8fc46b9209e27f7616d8f201ac63b837816d7d75
Yerin Pyeon <49026286+pmi4202@users.noreply.github.com>
2023-01-16 22:21:23 +0900

    [Silver I] Title: 지름길, Time: 80 ms, Memory: 11640 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.java
+++ 백준/Silver/1446. 지름길/지름길.java
@@ -1,10 +1,26 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    public  static class Road implements Comparable<Road>{
+        int start;
+        int end;
+        int distance;
+        public Road(int start, int end, int distance) {
+            this.start = start;
+            this.end = end;
+            this.distance = distance;
+        }
+
+        @Override
+        public int compareTo(Road o) {
+            return this.start - o.start;
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -15,23 +31,30 @@ public class Main {
         Arrays.fill(result, Integer.MAX_VALUE);
         result[0] = 0;
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[0] - o2[0]);
+        Road[] roads = new Road[N];
+
         for(int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine());
-            pq.add(new int[]{
+            roads[i] = new Road(
                     Integer.parseInt(st.nextToken()),
                     Integer.parseInt(st.nextToken()),
-                    Integer.parseInt(st.nextToken())
-            });
+                    Integer.parseInt(st.nextToken()));
         }
+        Arrays.sort(roads);
 
+        int idx = 0;
         for(int i=0; i<D; i++){
-            while(!pq.isEmpty() && pq.peek()[0] == i){
-                int[] now = pq.poll();
-                if(now[1] > D){continue;}
-                result[now[1]] = Math.min(result[now[1]], result[i] + now[2]);
+
+            while(idx < N && roads[idx].start == i) {
+                if(roads[idx].end > D) {
+                    idx++;
+                    continue;
+                }
+                result[roads[idx].end] = Math.min(result[roads[idx].end], result[i] + roads[idx].distance);
+                idx++;
             }
-            result[i+1] = Math.min(result[i+1], result[i] + 1);
+            result[i + 1] = Math.min(result[i + 1], result[i] + 1);
+
         }
         System.out.println(result[D]);
     }
