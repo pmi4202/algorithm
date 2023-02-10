ca4b2181ec1ecf73d159889c0bfa9523cc5465d9
Yerin Pyeon <49026286+pmi4202@users.noreply.github.com>
2023-02-10 21:23:56 +0900

    [Gold IV] Title: 가운데에서 만나기, Time: 304 ms, Memory: 25444 KB -BaekjoonHub

--- 백준/Gold/21940. 가운데에서 만나기/가운데에서 만나기.java
+++ 백준/Gold/21940. 가운데에서 만나기/가운데에서 만나기.java
@@ -3,7 +3,6 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-//도시의 개수 N과 도로의 개수M
 public class Main {
 
     public static void main(String[] args) throws Exception {
