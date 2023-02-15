6cb07e7d72f351cbeb34fb52064b713eb318cc1e
Yerin Pyeon <49026286+pmi4202@users.noreply.github.com>
2023-02-15 15:23:34 +0900

    [Gold IV] Title: 우유 도시, Time: 540 ms, Memory: 107624 KB -BaekjoonHub

--- 백준/Gold/14722. 우유 도시/우유 도시.java
+++ 백준/Gold/14722. 우유 도시/우유 도시.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -24,20 +23,10 @@ public class Main {
         }
 
         solve();
-//        for(int i=0; i<N; i++){
-//            for(int j=0; j<N; j++){
-//                System.out.print(Arrays.toString(result[i][j]) + " | ");
-//            }
-//            System.out.println();
-//        }
         System.out.println(Math.max(Math.max(result[N-1][N-1][0], result[N-1][N-1][1]), result[N-1][N-1][2]));
     }
 
     public static void solve(){
-        if(map[0][0] == 0){
-            result[0][0][0] = 1;
-        }
-
         for(int i=0; i<N; i++){
             for(int j=0; j<N; j++){
                 //위
@@ -53,33 +42,23 @@ public class Main {
                     }
 
                 }
-                //왼쪽
+                //왼
                 int y = j - 1;
                 if(0<=y && y<N){
-//                    if(map[i][j] == 0 && result[i][y][0] == 0){//이제 시작!
-//                        result[i][j][0] = 1;
-//                    }
-//                    else{
-                        for(int k=0; k<=2; k++){
-                            if(k==map[i][j] && result[i][y][(map[i][j] + 2)%3] > 0){
-                                int temp = Math.max(result[i][y][k], result[i][y][(map[i][j] + 2)%3] + 1);
-                                result[i][j][k] = Math.max(result[i][j][k], temp);
-                            }
-                            else{
-                                result[i][j][k] = Math.max(result[i][j][k], result[i][y][k]);
-                            }
-//                        if(i==3 && j==0){
-//                            System.out.println("here");
-//                            System.out.println(Arrays.toString(result[i][j]));
-//                        }
+                    for(int k=0; k<=2; k++){
+                        if(k==map[i][j] && result[i][y][(map[i][j] + 2)%3] > 0){
+                            int temp = Math.max(result[i][y][k], result[i][y][(map[i][j] + 2)%3] + 1);
+                            result[i][j][k] = Math.max(result[i][j][k], temp);
                         }
-//                    }
+                        else{
+                            result[i][j][k] = Math.max(result[i][j][k], result[i][y][k]);
+                        }
+                    }
 
                 }
-                if(map[i][j] == 0 && result[i][j][0] == 0){//이제 시작!
+                if(map[i][j] == 0 && result[i][j][0] == 0){
                     result[i][j][0] = 1;
                 }
-
 
             }
         }
