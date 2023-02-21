599b983a8486e01e9f72a999a82e293bd0c0e80a
Yerin Pyeon <49026286+pmi4202@users.noreply.github.com>
2023-02-21 17:02:08 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 324 ms, Memory: 22896 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().split(" ");
+        int N = Integer.parseInt(inputs[0]);
+        int K = Integer.parseInt(inputs[1]);
+
+        boolean[] visited = new boolean[100_001];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2)->o1[1]-o2[1]);
+        pq.add(new int[]{N, 0});
+
+         while(!pq.isEmpty()){
+            int[] now = pq.poll();
+            if(now[0] == K){
+                System.out.println(now[1]);
+                break;
+            }
+            if(visited[now[0]]){
+                continue;
+            }
+            visited[now[0]] = true;
+
+            if(now[0] - 1 >= 0 && !visited[now[0] - 1]){
+                pq.add(new int[]{now[0] - 1, now[1] + 1});
+            }
+            if(now[0] + 1 <= 100000 && !visited[now[0] + 1]){
+                pq.add(new int[]{now[0] + 1, now[1] + 1});
+            }
+            if(now[0] * 2 <= 100000 && !visited[now[0]*2]){
+                pq.add(new int[]{now[0]*2, now[1]});
+            }
+
+        }
+    }
+}
