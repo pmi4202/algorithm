e86f3393c8b49221254bf585fb172d5aa81afbdb
Yerin Pyeon <49026286+pmi4202@users.noreply.github.com>
2023-02-21 18:33:45 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 80 ms, Memory: 11904 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,7 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.PriorityQueue;
 
 public class Main {
 
@@ -16,18 +14,15 @@ public class Main {
             System.out.println(N-K);
         }
         else{
-            //N보다 작은 값은 빼기로 가는게 가장 빠름
             for(int i=0; i<N; i++){
                 dp[i] = N-i;
             }
             for(int i=N+1; i<=K; i++){
-                dp[i] = dp[i-1] + 1;
                 if(i%2==0){
                     dp[i] = Math.min(dp[i/2], dp[i-1] + 1);
                 }
                 else{
-                    int min = Math.min(dp[(i-1)/2] + 1, dp[(i+1)/2] + 1);
-                    dp[i] = Math.min(dp[i-1] + 1, min);
+                    dp[i] = Math.min(dp[i-1] + 1, dp[(i+1)/2] + 1);
                 }
 
             }
