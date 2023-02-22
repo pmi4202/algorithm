b30260925875bdd77a325c4092fdd97400c3dd0a
Yerin Pyeon <49026286+pmi4202@users.noreply.github.com>
2023-02-22 11:42:10 +0900

    [Silver II] Title: 결혼식, Time: 108 ms, Memory: 14540 KB -BaekjoonHub

--- 백준/Silver/5567. 결혼식/결혼식.java
+++ 백준/Silver/5567. 결혼식/결혼식.java
@@ -1,10 +1,12 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
         int N = Integer.parseInt(br.readLine());
         int L = Integer.parseInt(br.readLine());
         int result = 0;
@@ -12,9 +14,9 @@ public class Main {
         boolean[] visited = new boolean[N+1];
 
         for(int i=0; i<L; i++){
-            String[] inputs = br.readLine().split(" ");
-            int n1 = Integer.parseInt(inputs[0]);
-            int n2 = Integer.parseInt(inputs[1]);
+            st = new StringTokenizer(br.readLine());
+            int n1 = Integer.parseInt(st.nextToken());
+            int n2 = Integer.parseInt(st.nextToken());
             adjmatrix[n1][n2] = true;
             adjmatrix[n2][n1] = true;
         }
