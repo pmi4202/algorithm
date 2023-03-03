3fcd49d6ab306c9e8a386dfa3d4f732fda2ce725
Yerin Pyeon <49026286+pmi4202@users.noreply.github.com>
2023-03-03 14:54:54 +0900

    [Gold V] Title: 톱니바퀴, Time: 80 ms, Memory: 11604 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
@@ -14,12 +14,7 @@ public class Main {
         }
 
         public void turn(int dir){
-            if(dir == 1){//시계 방향
-                start = (start + 7)%8;
-            }
-            else{
-                start = (start + 1)%8;
-            }
+            start = (dir == 1) ? (start + 7)%8 : (start + 1)%8;
         }
 
         public int getTop(){
@@ -51,35 +46,41 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         while(N-- > 0){
             st = new StringTokenizer(br.readLine());
-            visited = new boolean[4];
             simulation(wheels, Integer.parseInt(st.nextToken())-1, Integer.parseInt(st.nextToken()));
         }
 
         int result = 0;
         for(int i=0; i<4; i++){
-            if(wheels[i].getTop() == 1){
-                result |= (1<<i);
-            }
+            result |= (wheels[i].getTop()<<i);
         }
         System.out.println(result);
     }
 
-    static boolean[] visited;
-    //no번 톱니바퀴를 dir방향으로 회전
     public static void simulation(Wheel[] wheels, int no, int dir){
-        visited[no] = true;
-        //왼쪽
-        int l = no - 1;
-        if(0 <= l && !visited[l] && wheels[no].getLeft() != wheels[l].getRight()){
-            simulation(wheels, l, dir*-1);
+        int prev = wheels[no].getLeft();
+        int d = dir;
+
+        for(int i=no-1; i>=0; i--){
+            if(prev == wheels[i].getRight()){
+                break;
+            }
+            d *= -1;
+            prev = wheels[i].getLeft();
+            wheels[i].turn(d);
         }
 
-        //오른쪽
-        int r = no + 1;
-        if(r < 4 && !visited[r] && wheels[no].getRight() != wheels[r].getLeft()){
-            simulation(wheels, r, dir*-1);
+        prev = wheels[no].getRight();
+        d = dir;
+        for(int i=no+1; i<4; i++){
+            if(prev == wheels[i].getLeft()){
+                break;
+            }
+            d = d * -1;
+            prev = wheels[i].getRight();
+            wheels[i].turn(d);
         }
 
         wheels[no].turn(dir);
+
     }
 }
