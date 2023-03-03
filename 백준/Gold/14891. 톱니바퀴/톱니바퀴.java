d18f6bb352d34d1adb766ceeaa009166f0561bb6
Yerin Pyeon <49026286+pmi4202@users.noreply.github.com>
2023-03-03 12:35:19 +0900

    [Gold V] Title: 톱니바퀴, Time: 80 ms, Memory: 11800 KB -BaekjoonHub

---
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
@@ -0,0 +1,95 @@
+import java.awt.image.BufferedImageFilter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static class Wheel{
+        int start;//12시방향 index
+        int[] ns;
+
+        public Wheel(){
+            start = 0;
+            ns = new int[8];
+        }
+
+        public void turn(int dir){
+            if(dir == 1){//시계 방향
+                start = (start + 7)%8;
+            }
+            else{
+                start = (start + 1)%8;
+            }
+        }
+
+        public int getTop(){
+            return ns[start];
+        }
+
+        public int getLeft(){
+            return ns[(start+6)%8];
+        }
+
+        public int getRight(){
+            return ns[(start+2)%8];
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        Wheel[] wheels = new Wheel[4];
+
+        for(int i=0; i<4; i++){
+            wheels[i] = new Wheel();
+            String input = br.readLine();
+            for(int j=0; j<8; j++){
+                wheels[i].ns[j] = input.charAt(j) - '0';
+            }
+        }
+
+        int N = Integer.parseInt(br.readLine());
+        while(N-- > 0){
+            st = new StringTokenizer(br.readLine());
+            simulation(wheels, Integer.parseInt(st.nextToken())-1, Integer.parseInt(st.nextToken()));
+        }
+
+        int result = 0;
+        for(int i=0; i<4; i++){
+            if(wheels[i].getTop() == 1){
+                result |= (1<<i);
+            }
+        }
+        System.out.println(result);
+    }
+
+    //no번 톱니바퀴를 dir방향으로 회전
+    public static void simulation(Wheel[] wheels, int no, int dir){
+        int prev = wheels[no].getLeft();
+        int d = dir;
+
+        for(int i=no-1; i>=0; i--){
+            if(prev == wheels[i].getRight()){
+                break;
+            }
+            d *= -1;
+            prev = wheels[i].getLeft();
+            wheels[i].turn(d);
+        }
+
+        prev = wheels[no].getRight();
+        d = dir;
+        for(int i=no+1; i<4; i++){
+            if(prev == wheels[i].getLeft()){
+                break;
+            }
+            d = d * -1;
+            prev = wheels[i].getRight();
+            wheels[i].turn(d);
+        }
+
+        wheels[no].turn(dir);
+
+    }
+}
