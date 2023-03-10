a767772f7a846df679ee369be137b88d299eda40
Yerin Pyeon <49026286+pmi4202@users.noreply.github.com>
2023-03-10 14:45:36 +0900

    Update 한 줄로 서기.java

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -1,32 +1,26 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N+1];
-        int[] result = new int[N];
+        List<Integer> result = new LinkedList<>();
 
         st = new StringTokenizer(br.readLine());
         for(int i=1; i<=N; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        for(int i=1; i<=N; i++){
-            int cnt = 0;
-            for(int j=0; j<N; j++){
-                if(result[j]==0){
-                    if(cnt++ == arr[i]){
-                        result[j] = i;
-                        break;
-                    }
-                }
-            }
+        for(int i=N; i>=1; i--){
+            result.add(arr[i], i);
         }
+
         StringBuilder sb = new StringBuilder();
         for(int su : result){
             sb.append(su).append(" ");
