1e1c6acbdc6e377cb899911865de092920f00ca8
Yerin Pyeon <49026286+pmi4202@users.noreply.github.com>
2023-03-25 13:39:17 +0900

    [Gold IV] Title: 좋다, Time: 136 ms, Memory: 12832 KB -BaekjoonHub

---
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N;
+    static long[] arr;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        arr = new long[N];
+        for(int i=0; i<N; i++){
+            arr[i] = Long.parseLong(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        int result = 0;
+        for(int i=0; i<N; i++){
+            if(canMake(i)) result++;
+        }
+        System.out.println(result);
+    }
+
+    public static boolean canMake(int idx){
+        int l = 0, r = N-1;
+        while(true){
+            if(l==idx) l++;
+            if(r==idx) r--;
+            if(l >= r) break;
+            long temp = arr[l] + arr[r];
+            if(temp == arr[idx]){
+                return true;
+            }
+            else if(temp < arr[idx]){
+                l++;
+            }
+            else{
+                r--;
+            }
+        }
+        return false;
+    }
+}
