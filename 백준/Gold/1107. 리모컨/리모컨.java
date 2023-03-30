55cb6019f63a8be699da97e94c5ecb6548dcc2e8
Yerin Pyeon <49026286+pmi4202@users.noreply.github.com>
2023-03-30 15:55:46 +0900

    [Gold V] Title: 리모컨, Time: 96 ms, Memory: 11884 KB -BaekjoonHub

---
+++ 백준/Gold/1107. 리모컨/리모컨.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static boolean[] broken;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        broken = new boolean[10];
+
+        if(M!=0) {
+            st = new StringTokenizer(br.readLine());
+            while (M-- > 0) {
+                broken[Integer.parseInt(st.nextToken())] = true;
+            }
+        }
+
+        if(N==100){
+            System.out.println(0);
+            return;
+        }
+        else{
+            int min = Math.abs(100 - N);//+, -로만 갈 때가 최대
+            for(int i=0; i<min; i++){
+                int l = N-i;
+                if(0 <= l){
+                    if(canPress(l)){
+                        min = Math.min(min, i + getLen(l));
+                        break;
+                    }
+                }
+                int r = N+i;
+                if(canPress(r)){
+                    min = Math.min(min, i + getLen(r));
+                    break;
+                }
+            }
+            System.out.println(min);
+        }
+
+    }
+
+    public static boolean canPress(int number){
+        while(number >= 10){
+            int btn = number%10;
+            if(broken[btn]) return false;
+            number /= 10;
+        }
+        return !broken[number];
+    }
+
+    public static int getLen(int number){
+        int cnt = 1;
+        while(number >= 10){
+            number /= 10;
+            cnt++;
+        }
+        return cnt;
+    }
+}
