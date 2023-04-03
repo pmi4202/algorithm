59c42ed0320b43e5210e0b8566777c7f2cf1b103
Yerin Pyeon <49026286+pmi4202@users.noreply.github.com>
2023-04-03 12:05:54 +0900

    [Silver I] Title: 회전 초밥, Time: 132 ms, Memory: 16340 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.java
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -1,4 +1,4 @@
-import java.io.BufferedReader;
+;import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
@@ -6,12 +6,12 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());//접시 수
-        st.nextToken();//가짓수
-        int K = Integer.parseInt(st.nextToken());//연속해서 먹는 수
-        int C = Integer.parseInt(st.nextToken());//쿠폰 번호
+        int N = Integer.parseInt(st.nextToken());
+        st.nextToken();
+        int K = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
         int[] plates = new int[N];
-        int[] record = new int[3001];//해당 번호의 음식 먹은 횟수
+        int[] record = new int[3001];//번호별 먹은 횟수
 
         for(int i=0; i<N; i++){
             plates[i] = Integer.parseInt(br.readLine());
@@ -27,23 +27,10 @@ public class Main {
 
         int result = cnt;
         if(N!=K){
-            int r = K;
-            for(int l=0; l<N-1; l++){
-                if(plates[l] != plates[r]){
-                    if(--record[plates[l]] == 0) {
-                        cnt--;
-                    }
-                    if(record[plates[r]]++ == 0) {
-                        cnt++;
-                    }
-                    result = Math.max(result, cnt);
-                    if(result == K+1) break;
-                }
-
-                if(++r >= N){
-                    r%=N;
-                }
-
+            for(int i=0; i<N-1; i++){
+                if(--record[plates[i]] == 0) cnt--;
+                if(record[plates[(i+K)%N]]++ == 0) cnt++;
+                result = Math.max(result, cnt);
             }
         }
 
