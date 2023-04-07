16f17727e5f60b81bc0d02cdfeea7a3d518b0e10
Yerin Pyeon <49026286+pmi4202@users.noreply.github.com>
2023-04-07 17:36:31 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 80 ms, Memory: 11604 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -4,14 +4,16 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N, result;
+    static int N;
     static int[][] map;
+    static int[][][] dp;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         N = Integer.parseInt(br.readLine());
         map = new int[N+1][N+1];
+        dp = new int[N+1][N+1][3];//가로, 세로. 대각선
 
         for(int i=1; i<=N; i++){
             st = new StringTokenizer(br.readLine());
@@ -20,32 +22,21 @@ public class Main {
             }
         }
 
-        dfs(1, 2, 0);
-        System.out.println(result);
-    }
-
-    public static void dfs(int x, int y, int dir){
-        if(x>N || y>N || map[x][y] == 1) return;
-        if(dir==2 && (map[x-1][y] == 1 || map[x][y-1] == 1)) return;
-        if(x==N && y==N){
-            result++;
-            return;
+        for(int i=2; i<=N; i++){
+            if(map[1][i] == 1) break;
+            dp[1][i][0] = 1;
         }
 
-        switch (dir){
-            case 0:
-                dfs(x, y+1, 0);
-                dfs(x+1, y+1, 2);
-                break;
-            case 1:
-                dfs(x+1, y, 1);
-                dfs(x+1, y+1, 2);
-                break;
-            case 2:
-                dfs(x, y+1, 0);
-                dfs(x+1, y, 1);
-                dfs(x+1, y+1, 2);
+        for(int i=2; i<=N; i++){
+            for(int j=2; j<=N; j++) {
+                if (map[i][j] == 1) continue;
+                dp[i][j][0] = dp[i][j - 1][0] + dp[i][j - 1][2];
+                dp[i][j][1] = dp[i - 1][j][1] + dp[i - 1][j][2];
+                if (map[i - 1][j] == 1 || map[i][j - 1] == 1) continue;
+                dp[i][j][2] = dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1] + dp[i - 1][j - 1][2];
+            }
         }
 
+        System.out.println(dp[N][N][0] + dp[N][N][1] + dp[N][N][2]);
     }
 }
