04b5c4cecca0d14d11d52ab298aa44bda2f92e37
Yerin Pyeon <49026286+pmi4202@users.noreply.github.com>
2023-04-11 21:54:33 +0900

    [Gold IV] Title: 방 번호, Time: 100 ms, Memory: 15036 KB -BaekjoonHub

--- 백준/Gold/15668. 방 번호/방 번호.java
+++ 백준/Gold/15668. 방 번호/방 번호.java
@@ -3,50 +3,34 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    static int N, result;
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        sb.append(-1);
 
-        for(int i=1; i<=9; i++){
-            dfs(N/2, i, 0 | (1<<i));
-        }
+        for(int i = Math.min(N/2, 98765); i>0; i--){
+            int j = N-i;
+            if(i==j) continue;
+            boolean[] used = new boolean[10];
 
-        if(result == 0){
-            System.out.println("-1");
-        }
-        else{
-            StringBuilder sb = new StringBuilder();
-            sb.append(result).append(" + ").append(N-result);
-            System.out.println(sb);
-        }
-    }
-
-    public static void dfs(int half, int su, int used){
-        if(su > half || result > 0){
-            return;
-        }
-
-        int n2 = N - su;
-        int visited = used;
-        while(n2 > 0){
-            int temp = n2%10;
-            if((visited & (1<<temp)) > 0){
+            if(check(i, used) && check(j, used)){
+                sb = new StringBuilder();
+                sb.append(i).append(" + ").append(j);
                 break;
             }
-            visited |= (1<<temp);
-            n2/=10;
         }
-        if(n2==0){
-            result = su;
-            return;
-        }
+        System.out.println(sb);
 
-        for(int i=0; i<=9; i++){
-            if((used & (1<<i)) > 0) continue;
-            dfs(half, su*10 + i, used | (1<<i));
-        }
     }
 
+    public static boolean check(int su, boolean[] used) {
+        while(su > 0){
+            int n = su%10;
+            if(used[n]) return false;
+            used[n] = true;
+            su/=10;
+        }
+        return true;
+    }
 }
