948e2adff5ae5a7bfacb02e940222bfaee8fd9df
Yerin Pyeon <49026286+pmi4202@users.noreply.github.com>
2023-04-12 13:53:47 +0900

    [Gold IV] Title: 부분합, Time: 224 ms, Memory: 22504 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+        int l = 0, r = 0, result = 100_001;
+        long sum = 0;
+        int[] arr = new int[N+1];
+        st = new StringTokenizer(br.readLine());
+        for(int i=1; i<=N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        while(l < N && r <= N){
+            sum -= arr[l];
+            l++;
+            while(r < N && sum < S){
+                sum += arr[++r];
+            }
+            if(sum >= S){
+                result = Math.min(result, r-l+1);
+            }
+        }
+        System.out.println((result > N) ? 0 : result);
+    }
+}
