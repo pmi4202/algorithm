9b09521ffb5b7775947d8da698a50dcc72d66bdc
Yerin Pyeon <49026286+pmi4202@users.noreply.github.com>
2023-04-14 21:53:55 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 168 ms, Memory: 43676 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -11,32 +10,32 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        int[] step = new int[200_000];
-        int[] cnt = new int[200_000];
-        Queue<int[]> q = new LinkedList<>();
-        Arrays.fill(step, Integer.MAX_VALUE);
-        q.add(new int[]{N, 0});//숫자, step
+        boolean[] visited = new boolean[200_000];
+        Queue<Integer> q = new LinkedList<>();
+        q.add(N);
 
-        while(!q.isEmpty()){
-            int[] now = q.poll();
-            if(step[now[0]] < now[1]){
-                continue;
-            }
-
-            step[now[0]] = now[1];
-            cnt[now[0]]++;
-            if(step[K] == Integer.MAX_VALUE){
-                if(now[0] > 0){
-                    q.add(new int[]{now[0] - 1, now[1] + 1});
+        int step = 0, cnt = 0;
+        outloop : while(!q.isEmpty()){
+            for(int i=0, size = q.size(); i<size; i++){
+                int now = q.poll();
+                visited[now] = true;
+                if(now == K){
+                    cnt++;
                 }
-                if(now[0] < K) {
-                    q.add(new int[]{now[0] + 1, now[1] + 1});
-                    q.add(new int[]{now[0] * 2, now[1] + 1});
+                if(now > 0 && !visited[now - 1]){
+                    q.add(now - 1);
+                }
+                if(now < K) {
+                    if(!visited[now + 1]) q.add(now + 1);
+                    if(!visited[now * 2]) q.add(now * 2);
                 }
             }
+            if(cnt > 0) break;
+            step++;
         }
+
         StringBuilder sb = new StringBuilder();
-        sb.append(step[K]).append("\n").append(cnt[K]);
+        sb.append(step).append("\n").append(cnt);
         System.out.println(sb);
     }
 }
