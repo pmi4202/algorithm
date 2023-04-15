de92d9419fe4b1a7151ab2b42b32425a7679aa1b
Yerin Pyeon <49026286+pmi4202@users.noreply.github.com>
2023-04-15 22:53:32 +0900

    [Silver II] Title: 이동하기, Time: 424 ms, Memory: 75932 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.java
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -9,12 +9,14 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int[][] map = new int[N+1][M+1];
+        
         for(int i=1; i<=N; i++){
             st = new StringTokenizer(br.readLine());
             for(int j=1; j<=M; j++){
                 map[i][j] = Math.max(map[i-1][j], map[i][j-1]) + Integer.parseInt(st.nextToken());
             }
         }
+        
         System.out.println(map[N][M]);
     }
 }
