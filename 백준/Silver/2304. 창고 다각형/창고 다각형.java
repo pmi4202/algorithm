19358313aa5d194495731fafdb40c7972b8c960a
Yerin Pyeon <49026286+pmi4202@users.noreply.github.com>
2023-04-17 18:16:25 +0900

    [Silver II] Title: 창고 다각형, Time: 88 ms, Memory: 11956 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.java
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.java
@@ -1,59 +1,39 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static class Item implements Comparable<Item> {
-        int idx, height;
-
-        public Item(int idx, int height){
-            this.idx = idx;
-            this.height = height;
-        }
-
-        @Override
-        public int compareTo(Item item){
-            return this.idx - item.idx;
-        }
-
-    }
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int N = Integer.parseInt(br.readLine());
-        Item[] items = new Item[N];
+        int[] arr = new int[1002];
+        int maxIdx = 0, max = 0;
         for(int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine());
-            items[i] = new Item(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+            int idx = Integer.parseInt(st.nextToken());
+            arr[idx] = Integer.parseInt(st.nextToken());
+            if(max < arr[idx]){
+                max = arr[idx];
+                maxIdx = idx;
+            }
         }
-        Arrays.sort(items);
-
-        //
-        Stack<Item> s = new Stack<>();
-        Item maxItem = new Item(0, 0);
-        s.add(maxItem);
 
         int result = 0;
-        for(Item item : items){
-            while(!s.isEmpty() && s.peek().height < item.height){
-                s.pop();
+        for(int i=1; i<=maxIdx; i++){
+            if(arr[i-1] > arr[i]){
+                arr[i] = arr[i-1];
             }
-            s.add(item);
-
-            if(maxItem.height < item.height){
-                result += (item.idx - maxItem.idx) * maxItem.height;
-                maxItem = item;
+            result += arr[i];
+        }
+        for(int i=1000; i>maxIdx; i--){
+            if(arr[i+1] > arr[i]){
+                arr[i] = arr[i+1];
             }
+            result += arr[i];
         }
-        //
-        while(s.size() > 1){
-            Item now = s.pop();
-            result += (now.idx - s.peek().idx) * now.height;
-        }
-        result += s.peek().height;
         System.out.println(result);
+
     }
 }
