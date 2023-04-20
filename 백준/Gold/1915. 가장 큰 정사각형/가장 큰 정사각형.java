fc974644b96f0d20bea9b9db204822bf7fdd08a7
Yerin Pyeon <49026286+pmi4202@users.noreply.github.com>
2023-04-20 16:13:37 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 264 ms, Memory: 21880 KB -BaekjoonHub

---
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] map = new int[N][M];
+
+        int result = 0;
+        for(int i=0; i<N; i++){
+            String input = br.readLine();
+            for(int j=0; j<M; j++){
+                map[i][j] = input.charAt(j)-'0';
+                if(map[i][j] == 1){result = 1;}
+            }
+        }
+
+        for(int i=1; i<N; i++){
+            for(int j=1; j<M; j++){
+                if(map[i][j] == 0){ continue;}
+                if(map[i-1][j] != 0 && map[i-1][j-1] != 0 && map[i][j-1] != 0){
+                    int min = Math.min(map[i-1][j], Math.min(map[i-1][j-1], map[i][j-1]));
+                    map[i][j] = min + 1;
+                    result = Math.max(result, map[i][j]);
+                }
+            }
+        }
+        System.out.println(result*result);
+    }
+}
