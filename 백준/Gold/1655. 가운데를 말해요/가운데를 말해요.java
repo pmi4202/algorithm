ba47fa0f8850a714309db428591b4600408314e7
Yerin Pyeon <49026286+pmi4202@users.noreply.github.com>
2023-04-21 15:15:12 +0900

    [Gold II] Title: 가운데를 말해요, Time: 348 ms, Memory: 31520 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
@@ -7,27 +7,27 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((o1, o2) -> o2 - o1);
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>();
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
 
-        minHeap.add(Integer.parseInt(br.readLine()));
-        sb.append(minHeap.peek()).append("\n");
+        maxHeap.add(-Integer.parseInt(br.readLine()));
+        sb.append(-maxHeap.peek()).append("\n");
         for(int i=2; i<=N; i++){
             int num = Integer.parseInt(br.readLine());
-            if(minHeap.peek() <= num){
+            if(-maxHeap.peek() < num){
                 minHeap.offer(num);
                 if(minHeap.size() > maxHeap.size()){
-                    maxHeap.offer(minHeap.poll());
+                    maxHeap.offer(-minHeap.poll());
                 }
             }
             else{
-                maxHeap.offer(num);
+                maxHeap.offer(-num);
                 if(maxHeap.size() > minHeap.size()+1){
-                    minHeap.offer(maxHeap.poll());
+                    minHeap.offer(-maxHeap.poll());
                 }
             }
 
-            sb.append(maxHeap.peek()).append("\n");
+            sb.append(-maxHeap.peek()).append("\n");
         }
         System.out.println(sb);
     }
