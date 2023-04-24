df353aa20c4e1361e2c26150cfc20e60786b60c4
Yerin Pyeon <49026286+pmi4202@users.noreply.github.com>
2023-04-24 14:43:31 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 728 ms, Memory: 128056 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -9,12 +9,10 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int[][] map = new int[N+1][N+1];
-        int[][] dp = new int[N+1][N+1];
         for(int i=1; i<=N; i++){
             st = new StringTokenizer(br.readLine());
             for(int j=1; j<=N; j++){
-                map[i][j] = Integer.parseInt(st.nextToken());
-                dp[i][j] = dp[i][j-1] + dp[i-1][j] - dp[i-1][j-1] + map[i][j];
+                map[i][j] = Integer.parseInt(st.nextToken()) + map[i][j-1] + map[i-1][j] - map[i-1][j-1];
             }
         }
         //
@@ -25,7 +23,7 @@ public class Main {
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
-            sb.append(dp[x2][y2]-dp[x2][y1-1] - dp[x1-1][y2] + dp[x1-1][y1-1]).append("\n");
+            sb.append(map[x2][y2] - map[x2][y1-1] - map[x1-1][y2] + map[x1-1][y1-1]).append("\n");
         }
         System.out.println(sb);
     }
