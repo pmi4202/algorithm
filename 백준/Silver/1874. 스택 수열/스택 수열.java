4fac62248b7ed05f86c75d29d0778ef6f702701f
Yerin Pyeon <49026286+pmi4202@users.noreply.github.com>
2023-05-17 19:51:01 +0900

    [Silver II] Title: 스택 수열, Time: 248 ms, Memory: 25168 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,30 +1,29 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine()), top = -1;
         int[] arr = new int[N];
+        int[] stack = new int[N];
         for(int i=0; i<N; i++){
             arr[i] = Integer.parseInt(br.readLine());
         }
-        Stack<Integer> stack = new Stack<>();
+
         int now = 1;
         for(int su : arr){
             while(now <= su){
-                stack.push(now);
+                stack[++top] = now++;
                 sb.append("+\n");
-                now++;
             }
-            if(su != stack.peek()){
+            if(su != stack[top]){
                 sb = new StringBuilder();
                 sb.append("NO");
                 break;
             }
-            stack.pop();
+            top--;
             sb.append("-\n");
         }
         System.out.println(sb);
