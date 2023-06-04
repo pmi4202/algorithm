092532efdb5487116868de703f7a171b8560431d
Yerin Pyeon <49026286+pmi4202@users.noreply.github.com>
2023-06-04 16:04:15 +0900

    [Silver II] Title: 랭킹전 대기열, Time: 88 ms, Memory: 11880 KB -BaekjoonHub

--- 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.java
+++ 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.java
@@ -1,5 +1,4 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -7,78 +6,59 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+
+    static class Player implements Comparable<Player> {
+        int lv;
+        String name;
+
+        public Player(int lv, String name){
+            this.lv = lv;
+            this.name = name;
+        }
+
+        @Override
+        public int compareTo(Player p){
+            return this.name.compareTo(p.name);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int pCount = Integer.parseInt(st.nextToken());
-        int roomMax = Integer.parseInt(st.nextToken());
+        List<List<Player>> rooms = new ArrayList<>();
+        int p = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        List<Room> rooms = new ArrayList<>();
-
-        for (int i = 0; i < pCount; i++) {
+        boolean done;
+        while(p-- > 0){
+            done = false;
             st = new StringTokenizer(br.readLine());
-            int level = Integer.parseInt(st.nextToken());
-            String nickname = st.nextToken();
-
-            boolean isInsert = false;
-            for (int j = 0; j < rooms.size(); j++) {
-                Room room = rooms.get(j);
-
-                if (room.start <= level && room.end >= level && room.users.size() != roomMax) {
-                    room.users.add(new User(level, nickname));
-                    isInsert = true;
+            Player player = new Player(Integer.parseInt(st.nextToken()), st.nextToken());
+            for(List<Player> room : rooms){
+                if(room.size() < m && Math.abs(room.get(0).lv-player.lv) <= 10){
+                    room.add(player);
+                    done = true;
                     break;
                 }
             }
-
-            if (!isInsert) {
-                Room room = new Room(level - 10, level + 10);
-                room.users.add(new User(level, nickname));
-                rooms.add(room);
+            if(!done){
+                List createdRoom = new ArrayList();
+                createdRoom.add(player);
+                rooms.add(createdRoom);
             }
         }
 
-        for (int i = 0; i < rooms.size(); i++) {
-            Room room = rooms.get(i);
-            List<User> users = room.users;
-            Collections.sort(users);
+        StringBuilder sb = new StringBuilder();
+        for(List<Player> room : rooms){
+            if(room.size() == m) sb.append("Started!\n");
+            else sb.append("Waiting!\n");
 
-            if (users.size() != roomMax) {
-                System.out.println("Waiting!");
-            } else {
-                System.out.println("Started!");
-            }
-
-            for (int j = 0; j < users.size(); j++) {
-                User user = users.get(j);
-                System.out.println(user.level + " " + user.nickname);
+            Collections.sort(room);
+            for(Player player : room){
+                sb.append(player.lv).append(" ")
+                        .append(player.name).append("\n");
             }
         }
-    }
-
-    static class Room {
-        public Room(int start, int end) {
-            this.start = start;
-            this.end = end;
-        }
-
-        int start;
-        int end;
-
-        List<User> users = new ArrayList<>();
-    }
-
-    static class User implements Comparable<User> {
-        public User(int level, String nickname) {
-            this.level = level;
-            this.nickname = nickname;
-        }
-
-        int level;
-        String nickname;
-
-        public int compareTo(User other) {
-            return this.nickname.compareTo(other.nickname);
-        }
+        System.out.println(sb);
     }
 }
