3c41df5cee913f8f5e289d6b6a819e514fcb02db
Yerin Pyeon <49026286+pmi4202@users.noreply.github.com>
2023-06-04 15:44:08 +0900

    [Silver II] Title: 랭킹전 대기열, Time: 240 ms, Memory: 18772 KB -BaekjoonHub

--- 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.java
+++ 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.java
@@ -19,31 +19,10 @@ public class Main {
         }
     }
 
-    static class Room{
-        int space;
-        List<Player> players;
-
-        public Room(int m){
-            space = m;
-            players = new ArrayList<>();
-        }
-
-        boolean join(Player p){
-            if(players.size() > 0){
-                if(space == 0 || Math.abs(players.get(0).level-p.level) > 10){
-                    return false;
-                }
-            }
-            players.add(p);
-            space--;
-            return true;
-        }
-    }
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        List<Room> rooms = new ArrayList<>();
+        List<List<Player>> rooms = new ArrayList<>();
         int p = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
@@ -51,24 +30,24 @@ public class Main {
             boolean done = false;
             st = new StringTokenizer(br.readLine());
             Player player = new Player(Integer.parseInt(st.nextToken()), st.nextToken());
-            for(Room room : rooms){
-                if(room.join(player)){
+            for(List<Player> room : rooms){
+                if(room.size() < m && Math.abs(room.get(0).level-player.level) <= 10){
+                    room.add(player);
                     done = true;
                     break;
                 }
             }
             if(!done){
-                Room createdRoom = new Room(m);
-                createdRoom.join(player);
+                List createdRoom = new ArrayList();
+                createdRoom.add(player);
                 rooms.add(createdRoom);
             }
         }
 
         StringBuilder sb = new StringBuilder();
-        for(Room room : rooms){
-            sb.append((room.space==0) ? "Started!\n" : "Waiting!\n");
-            Collections.sort(new ArrayList(room.players));
-            room.players.stream().sorted().forEach((Player player)->{
+        for(List<Player> room : rooms){
+            sb.append((room.size()==m) ? "Started!\n" : "Waiting!\n");
+            room.stream().sorted().forEach((Player player)->{
                 sb.append(player.level).append(" ")
                         .append(player.nickname).append("\n");
             });
