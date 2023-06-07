a8575cc51e1dcfa3d9eb570009c90922a74ab4be
Yerin Pyeon <49026286+pmi4202@users.noreply.github.com>
2023-06-07 23:15:18 +0900

    [Silver III] Title: 파일 정리, Time: 576 ms, Memory: 77744 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.java
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -1,23 +1,25 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Map<String, Integer> map = new HashMap<>();
+        Map<String, Integer> map = new TreeMap<>();
         StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
         String input;
         while(N-- > 0){
-            input = br.readLine().split("[.]")[1];
-            map.put(input, map.getOrDefault(input, 0)+1);
+            input = br.readLine();
+            String extension = input.substring(input.indexOf(".")+1);
+            map.put(extension, map.getOrDefault(extension, 0)+1);
         }
 
-        map.keySet().stream().sorted().forEach((key)->{
-            sb.append(key).append(" ").append(map.get(key)).append("\n");
-        });
+        for(String extension : map.keySet()){
+            sb.append(extension).append(" ")
+                    .append(map.get(extension)).append("\n");
+        }
         System.out.println(sb);
     }
 }
