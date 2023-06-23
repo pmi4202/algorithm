d06c314eecb3f9974cb278d7ec916619f49c6737
Yerin Pyeon <49026286+pmi4202@users.noreply.github.com>
2023-06-23 17:06:27 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.05 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -3,36 +3,36 @@ class Solution {
         int answer = 0;
         int prev = 0, now = 0;
         for(int i=0, size = dartResult.length(); i<size; i++){
-            now = dartResult.charAt(i) - '0';
-            if(dartResult.charAt(i+1) == '0'){
-                now*=10;
-                i++;
-            }
-            
-            switch(dartResult.charAt(++i)){
-                case 'T':
-                    now = (int)Math.pow(now, 3);
-                    break;
-                case 'D':
-                    now*=now;
-                    break;
-            }
-            
-            if(i+1<size){
-                if(dartResult.charAt(i+1)=='#'){
-                    now*=-1;
-                    i++;
-                }
-                else if(dartResult.charAt(i+1)=='*'){
-                    prev*=2;
-                    now*=2;
+            char op = dartResult.charAt(i);
+            //숫자라면,
+            if('0' <= op && op <= '9'){
+                answer += prev;
+                prev = now;
+                
+                now = op - '0';
+                if(dartResult.charAt(i+1) == '0'){
+                    now*=10;
                     i++;
                 }
             }
-            answer += prev;
-            prev = now;
+            else{
+                switch(op){
+                    case 'T':
+                        now = (int)Math.pow(now, 3);
+                        break;
+                    case 'D':
+                        now*=now;
+                        break;
+                    case '*':
+                        prev*=2;
+                        now*=2;
+                        break;
+                    case '#':
+                        now*=-1;
+                }
+            }
         }
-        answer += prev;
+        answer += prev + now;
         
         return answer;
     }
