4803ca90b15a8d778f9657d7c458b25ee6a8df56
Yerin Pyeon <49026286+pmi4202@users.noreply.github.com>
2023-06-26 15:39:27 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.10 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,44 +1,50 @@
+import java.util.*;
+
 class Solution {
     
+    int[] answer;
     int[] dx = {-1, 0, 1, 0}, dy = {0, 1, 0, -1};
+    boolean[][] visited;
     
     public int[] solution(String[][] places) {
-        int[] answer = new int[5];
+        answer = new int[5];
+        visited = new boolean[5][5];
         
+        Arrays.fill(answer, 1);
+
         for(int i=0; i<5; i++){
-            answer[i] = isFine(places[i]);    
+            for(int j=0; j<5; j++){
+                for(int k=0; k<5; k++){
+                    if(places[i][j].charAt(k)=='P'){
+                        visited[j][k] = true;
+                        dfs(i, j, k, 0, places[i]);
+                        visited[j][k] = false;
+                    }
+                }
+            }   
         }
         
         return answer;
     }
     
-    public int isFine(String[] map){
-        for(int i=0; i<5; i++){
-            for(int j=0; j<5; j++){
-                if(map[i].charAt(j)=='P'){
-                    if(!check(map, i, j)) return 0;
-                }
-            }
+    public void dfs(int no, int x, int y, int count, String[] place){
+        if(count > 2) return;
+        if(count > 0 && place[x].charAt(y) == 'P'){
+            answer[no] = 0;
+            return;
         }
-        return 1;
-    }
-    
-    public boolean check(String[] map, int x, int y){
-        for(int i=1; i<4; i++){
+        
+        for(int i=0; i<4; i++){
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if(nx<0 || nx>=5 || ny<0 || ny>=5 || map[nx].charAt(ny) =='X') continue;
+            if(nx<0 || nx>=5 || ny<0 || ny>=5 || visited[nx][ny] || place[nx].charAt(ny) == 'X') continue;
+            visited[nx][ny] = true;
+            dfs(no, nx, ny, count+1, place);
+            visited[nx][ny] = false;
             
-            if(map[nx].charAt(ny) == 'P') return false;
-            else{
-                for(int j=-1; j<=1; j++){
-                    int mx = nx + dx[(i+j)%4];
-                    int my = ny + dy[(i+j)%4];
-                    if(mx<0 || mx>=5 || my<0 || my>=5) continue;
-                    if(map[mx].charAt(my)=='P') return false;
-                }
-            }
         }
-        return true;
+        
     }
+    
+    
 }
