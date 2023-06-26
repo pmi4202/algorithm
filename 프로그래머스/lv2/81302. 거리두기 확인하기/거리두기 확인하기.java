e7ae426175764041b196897b0ab627eb37abc05f
Yerin Pyeon <49026286+pmi4202@users.noreply.github.com>
2023-06-26 15:54:07 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.07 ms, Memory: 69.1 MB -BaekjoonHub

--- 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Solution {
     
     int[] answer;
-    int[] dx = {-1, 0, 1, 0}, dy = {0, 1, 0, -1};
+    int[] dx = {0, 1, 0}, dy = {1, 0, -1};
     boolean[][] visited;
     
     public int[] solution(String[][] places) {
@@ -34,7 +34,7 @@ class Solution {
             return;
         }
         
-        for(int i=0; i<4; i++){
+        for(int i=0; i<3; i++){
             int nx = x + dx[i];
             int ny = y + dy[i];
             if(nx<0 || nx>=5 || ny<0 || ny>=5 || visited[nx][ny] || place[nx].charAt(ny) == 'X') continue;
