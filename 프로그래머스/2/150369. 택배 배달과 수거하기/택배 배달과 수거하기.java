3aea56152a851c5a9dfa141c826b1fedbceac3ea
Yerin Pyeon <49026286+pmi4202@users.noreply.github.com>
2023-11-01 15:38:38 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 8.18 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -7,24 +7,18 @@ class Solution {
         int delIdx = n-1, pickIdx = n-1;
         
         while(delIdx != -1 || pickIdx != -1){
-            delIdx = getNextPos(deliveries, delIdx);
-            pickIdx = getNextPos(pickups, pickIdx);
+            while(delIdx >= 0 && deliveries[delIdx] == 0) delIdx--;
+            while(pickIdx >= 0 && pickups[pickIdx] == 0) pickIdx--;
             
             int temp = Math.max(delIdx, pickIdx) + 1;
-            answer += temp*2;
+            answer += temp;
             
             updateStatus(cap, deliveries, delIdx);
             updateStatus(cap, pickups, pickIdx);
         }
         
             
-        return answer;
-    }
-    
-    public int getNextPos(int[] arr, int idx){
-        while(idx >=0 && arr[idx] == 0) idx--;
-        
-        return idx;
+        return answer*2;
     }
     
     public int updateStatus(int cap, int[] arr, int idx){
