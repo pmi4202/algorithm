47d9741b3d0b17840f70b15fc5d766a5fd893d6c
Yerin Pyeon <49026286+pmi4202@users.noreply.github.com>
2024-03-16 14:56:40 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.77 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,58 +1,25 @@
 import java.util.*;
 
-class Date {
-    int year, mon, day;
-    
-    public Date(String date){
-        StringTokenizer st = new StringTokenizer(date, ".");
-        this.year = Integer.parseInt(st.nextToken());
-        this.mon = Integer.parseInt(st.nextToken());
-        this.day = Integer.parseInt(st.nextToken());
-    }
-    
-    public void toLast(int period){
-        int total = mon + period;
-        if(total % 12 == 0){
-            this.mon = 12;
-            this.year += (total - 12)/12;
-        } else{
-            this.mon = total % 12;
-            this.year += total / 12;
-        }
-    }
-    
-    public int compareTo(Date o){
-        if(this.year == o.year){
-            return (this.mon == o.mon) ? this.day - o.day : this.mon - o.mon;
-        } else{
-            return this.year - o.year;
-        }
-    }
-    
-}
-
 class Solution {
     
     public List<Integer> solution(String today, String[] terms, String[] privacies) {
-        List<Integer> answer = new ArrayList<>();
-        
         //1. 약관에 대한 유효기간
         int[] period = new int[26];
         
         for(String term : terms){
             StringTokenizer st = new StringTokenizer(term);
-            period[st.nextToken().charAt(0)-'A'] = Integer.parseInt(st.nextToken());
+            period[st.nextToken().charAt(0)-'A'] = Integer.parseInt(st.nextToken()) * 28;
         }
         
         //2.
-        Date todayDate = new Date(today);
+        List<Integer> answer = new ArrayList<>();
+        int todayDays = getDayOf(today);
         
         for(int i=0, size = privacies.length; i<size; i++){
             StringTokenizer st = new StringTokenizer(privacies[i]);
-            Date now = new Date(st.nextToken());
-            now.toLast(period[st.nextToken().charAt(0)-'A']);
+            int deadline = getDayOf(st.nextToken()) + period[st.nextToken().charAt(0)-'A'];
             
-            if(todayDate.compareTo(now) >= 0){
+            if(deadline <= todayDays){
                 answer.add(i+1);
             }
         }
@@ -60,4 +27,13 @@ class Solution {
         
         return answer;
     }
+    
+    public int getDayOf(String date){
+        StringTokenizer st = new StringTokenizer(date, ".");
+        int year = Integer.parseInt(st.nextToken());
+        int month = Integer.parseInt(st.nextToken());
+        int day = Integer.parseInt(st.nextToken());
+        return (year*12 + month)*28 + day;
+    }
+    
 }
