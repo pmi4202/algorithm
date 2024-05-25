a6708681281e6ded03b263ee149e8e8e9bc5fa1f
Yerin Pyeon <49026286+pmi4202@users.noreply.github.com>
2024-05-25 15:15:17 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.03 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -1,18 +1,21 @@
 class Solution {
     
     public int solution(int storey) {
-        return getResult(storey, 1, 0);
+        int result = 0;
+        while(storey != 0){
+            int upper = (storey%100)/10;
+            int now = storey%10;
+            
+            if(now>5 || now==5 && upper>=5){
+                storey+=10;
+                result+=(10-now);
+            } else{
+                result+=now;
+            }
+            
+            storey/=10;
+        }
+        return result;
     }
     
-    public int getResult(int storey, int idx, int cnt){
-        if(storey < 0) return Integer.MAX_VALUE;
-        if(storey == 0) return cnt;
-        
-        //+
-        int n = storey%(idx*10);
-        int plusResult = getResult(storey+(idx*10)-n, idx*10, cnt+(10-n/idx));
-        //-
-        int minusResult = getResult(storey-n, idx*10, cnt+n/idx);
-        return Math.min(plusResult, minusResult);
-    }
 }
